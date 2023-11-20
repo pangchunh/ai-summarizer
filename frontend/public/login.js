5df4d5eac8f9a95c9860e677f81805403550bc40
Chun <peterpang1103@gmail.com>
2023-11-19 22:04:06 -0800

    login page with register function, added middleware to authenicate user using jwt

---
+++ frontend/public/login.js
@@ -0,0 +1,110 @@
+const host = 'http://localhost:3000'
+
+let authToken = null; // Declare a global variable to store the token
+
+
+//handle form submit
+document.getElementById('loginForm').addEventListener('submit', (e) => {
+  e.preventDefault()
+  login()
+}
+)
+
+document.getElementById('registerForm').addEventListener('submit', (e) => {
+  e.preventDefault()
+  register()
+}
+)
+
+//populate register form when user click on the register link
+document.getElementById('registerLink').addEventListener('click', (e) => {
+  e.preventDefault()
+  document.getElementById('loginForm').style.display = 'none'
+  document.getElementById('registerForm').style.display = 'block'
+})
+
+
+
+async function login() {
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+  console.log("clicked")
+
+  const result = await fetch(`${host}/api/v1/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({username, password})
+  })
+  const data = await result.json()
+  console.log(`data: ${data.message}, data.token: ${data.token}, data.user: ${data.user}`)
+  //if success, redirect user and set cookie
+  if (data.token){
+    authToken = data.token;
+    document.cookie = `token=${data.token}`
+    //redirect to main.html by calling get request from server
+    const headers = new Headers();
+    headers.append('Authorization', `Bearer ${authToken}`);
+    window.location.href = `${host}/mainpage`
+
+  } else{
+    document.getElementById('error').innerText = data.message
+  }
+  
+}
+
+async function goToMainPage() {
+  const cookies = document.cookie.split(';'); 
+  const tokenCookie = cookies.find(cookie => cookie.trim().startsWith('token='));
+
+  
+  if (!tokenCookie) {
+    console.error('JWT token not found');
+    return;
+  }
+  const token = tokenCookie.split('=')[1];
+  console.log(`token: ${token}`)
+  
+  const result = await fetch(`${host}/mainpage`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'text/html',
+      'Authorization': `Bearer ${token}`
+    }
+  })
+}
+
+
+
+
+async function register() {
+  const username = document.getElementById('registerUserName').value;
+  const email = document.getElementById('registerEmail').value;
+  const password = document.getElementById('registerPassword').value;
+
+  // Add user if not exists in DB
+  const result = await fetch('http://localhost:3000/api/v1/create-user', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({username, email, password})
+  })
+
+  const data = await result.json()
+
+  //if success, redirect user and set cookie
+  if (data.token){
+    document.cookie = `token=${data.token}`
+    window.location.href = '/main.html'
+  } else{
+    document.getElementById('error').innerText = data.message
+  }
+
+}
+
+async function logout() {
+  document.cookie = `token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`
+  window.location.href = '/login.html'
+}
---
+++ services/login/app.js
@@ -0,0 +1,141 @@
+require('dotenv').config()
+const express = require("express");
+const cors = require('cors')
+const app = express();
+const PORT = process.env.PORT || 3000;
+const db = require('pg-promise')()(process.env.DATABASE_URL);
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const path = require('path');
+const cookieParser = require('cookie-parser')
+
+
+const host = process.env.HOST || 'http://localhost:3000'
+
+app.use(express.json(), cors(), cookieParser())
+app.use(express.static(path.join(__dirname, '../../frontend/public')))
+
+
+//middle for authentication
+const authenticateAPI = async (req, res, next) => {
+  const authHeader = req.headers.authorization
+  
+  //check if cookie token exists
+
+  if (!authHeader && !req.cookies.token) {
+    res.status = 401
+    return res.json({ "message": "No token provided" })
+  }
+  console.log(`authheader: ${authHeader}`)
+  const token = authHeader.split(' ')[1]
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    console.log(decoded)
+    const { uid } = decoded
+    console.log(`uid: ${uid}`)
+    const user = await db.oneOrNone(`select * from "user" where uid = '${uid}'`)
+    if (!user) {
+      res.status = 401
+      return res.json({ "message": "User not find, please retry" })
+    }
+    req.user = user
+    next()
+  } catch (error) {
+    res.status = 401
+    return res.json({ "message": `Invalid token: ${error}` })
+  }
+}
+
+
+const authenticatePage = async (req, res, next) => {
+  const { token } = req.cookies
+  //if no token, redirect to login page
+  if (!token) {
+    res.redirect('/')
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    const { uid } = decoded
+    const user = await db.oneOrNone(`select * from "user" where uid = '${uid}'`)
+    if (!user) {
+      res.redirect('/')
+    }
+    next()
+  } catch (error) {
+    res.redirect('/')
+  }
+}
+
+
+//create route to display main page using middleware
+app.get("/mainpage", authenticatePage, (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/public/main.html"))
+})
+
+//create route to display login page
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/public/login.html"))
+})
+
+
+app.post("/api/v1/login", async (req, res) => {
+  const { username, password } = req.body
+  try {
+    //retrieve user base on username
+    const user = await db.oneOrNone(`select * from "user" where username like '${username}'`)
+
+    if (!user) {
+      res.status = 401
+      return res.json({ "message": "User not find, please retry" })
+    }
+    const passwordMatch = await bcrypt.compare(password, user.password)
+    if (!passwordMatch) {
+      res.status = 401
+      return res.json({ "message": "Password incorrect, please retry" })
+    }
+    //create token with user id and send back to user
+    const token = jwt.sign({ uid: user.uid }, process.env.JWT_SECRET)
+
+
+
+    res.status = 200
+    res.json({ "message": "User logged in", user, token })
+
+  } catch (error) {
+    res.send(error)
+
+
+  }
+})
+
+app.post("/api/v1/create-user", async (req, res) => {
+  //create a user in the database by hasing the password in the body
+  const { username, email, password } = req.body
+  const hashedPassword = await bcrypt.hash(password, 10)
+  try {
+    const user = await db.oneOrNone(`select * from "user" where username like '${username}'`)
+    if (user) {
+      res.status = 401
+      return res.json({ "message": "User already exists, please retry" })
+    }
+    const newUser = await db.one(`insert into "user" (username, email, password) values ('${username}', '${email}', '${hashedPassword}') returning *`)
+    res.status = 201
+    res.json({ "message": "User created", newUser })
+  } catch (error) {
+    res.send(error)
+  }
+
+})
+
+app.listen(PORT, () => {
+  console.log(`App listening at ${PORT}`)
+})
+
+
+
+//sql query to create user db with uid, username, password, email
+
+
+
+
