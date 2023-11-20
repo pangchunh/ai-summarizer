bd377098bd44fecabe3b3e016457ce87b55c0c67
Chun <peterpang1103@gmail.com>
2023-11-19 17:34:13 -0800

    refactor project structure into different services

---
+++ frontend/index.js
@@ -0,0 +1,23 @@
+// <!--
+//   This example requires some changes to your config:
+  
+//   ```
+//   // tailwind.config.js
+//   module.exports = {
+//     // ...
+//     plugins: [
+//       // ...
+//       require('@tailwindcss/forms'),
+//     ],
+//   }
+//   ```
+// -->
+// <!--
+//   This example requires updating your template:
+
+//   ```
+//   <html class="h-full bg-white">
+//   <body class="h-full">
+//   ```
+// -->
+
